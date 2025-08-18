0f835847b40ba6ed122f4fd282cc0c9c18cdc5c8
Maithili-007 <patelmaithili55@gmail.com>
2025-08-18 14:45:34 +0530

    changed port

--- jd-analysis-service/app.py
+++ jd-analysis-service/app.py
@@ -59,5 +59,5 @@ def extract():
         traceback.print_exc() 
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
